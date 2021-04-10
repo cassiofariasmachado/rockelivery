cfcb34ccc3dad356d1dd8258852d65f4b719a1cc
Cássio Farias Machado <cassiofariasmachado@yahoo.com>
2021-04-10 09:58:00 -0300

    Add report creation

---
+++ lib/rockelivery/orders/report.ex
@@ -0,0 +1,37 @@
+defmodule Rockelivery.Orders.Report do
+  import Ecto.Query
+
+  alias Rockelivery.{Order, Item, Repo}
+  alias Rockelivery.Orders.TotalPrice
+
+  @default_block_size 500
+
+  def create(file_name \\ "report.csv") do
+    query = from order in Order, order_by: order.user_id
+
+    {:ok, order_list} =
+      Repo.transaction(
+        fn ->
+          query
+          |> Repo.stream(max_rows: @default_block_size)
+          |> Stream.chunk_every(@default_block_size)
+          |> Stream.flat_map(fn chunk -> Repo.preload(chunk, :items) end)
+          |> Enum.map(&parse_line/1)
+        end,
+        timeout: :infinity
+      )
+
+    File.write(file_name, order_list)
+  end
+
+  defp parse_line(%Order{user_id: user_id, payment_method: payment_method, items: items}) do
+    items_string = Enum.map(items, &item_string/1)
+    total_price = TotalPrice.calculate(items)
+
+    "#{user_id},#{payment_method},#{items_string}#{total_price}\n"
+  end
+
+  defp item_string(%Item{category: category, description: description, price: price}) do
+    "#{category},#{description},#{price},"
+  end
+end
